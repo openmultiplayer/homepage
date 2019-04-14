6d42e037efd534a41c40888acaa08325c7b96080
Barnaby Keene <hello@southcla.ws>
2019-04-14 22:33:54 +0100

    added a link to Y_Less' thread

--- pages/index.jsx
+++ pages/index.jsx
@@ -26,6 +26,10 @@ const Index = () => (
           and, in addition to this, many bugs will also be fixed within the server software without the need for hacks
           and workarounds.
         </p>
+        <p>
+          If you're wondering when the public release is planned or how you can help contribute to the project, please
+          see <a href="https://www.burgershot.gg/showthread.php?tid=99">this forum thread</a> for more information.
+        </p>
         <hr />
         <p>
           <span className="icon">
