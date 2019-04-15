f611341bf0c44d700732e2f0cff989d15f59f3aa
Barnaby Keene <hello@southcla.ws>
2019-04-15 20:42:43 +0100

    implemented the translations for the faq page

--- pages/faq.jsx
+++ pages/faq.jsx
@@ -1,23 +1,30 @@
 import React from "react";
-import Head from "next/head";
+import { useState } from "react";
 
 import { HeadContent } from "../components/HeadContent";
 import Wordmark from "../components/icons/Wordmark";
 
-import Faq from "../language/English";
+import { loadLanguages } from "../components/languages";
 
-export default () => (
-  <div className="container">
-    <HeadContent title="FAQ" />
+export default () => {
+  let [language, setLanguage] = useState("🇬🇧");
 
-    <main>
-      <header className="header">
-        <Wordmark width={300} height={"100%"} stroke="#d1cec8" background="#161f2b" />
-      </header>
-      <section className="content">
-        <Faq />
-        <hr />
-      </section>
-    </main>
-  </div>
-);
+  let languages = loadLanguages();
+  let currentLanguage = languages[language];
+
+  return (
+    <div className="container">
+      <HeadContent title="FAQ" />
+
+      <main>
+        <header className="header">
+          <Wordmark width={300} height={"100%"} stroke="#d1cec8" background="#161f2b" />
+        </header>
+        <section className="content">
+          {currentLanguage.faq()}
+          <hr />
+        </section>
+      </main>
+    </div>
+  );
+};
