4ef45a2cd16c6c5361ecb1fadea6b6185a5863db
Mel-vin <anmelvinn@gmail.com>
2019-04-16 01:51:58 +0200

    add dutch translation

--- language/Dutch.jsx
+++ language/Dutch.jsx
@@ -4,14 +4,18 @@ export const BODY = () => (
   <div>
     <h1>Open Multiplayer</h1>
     <p>
-      Een in ontwikkeling zijnde multiplayer mod voor <em>Grand Theft Auto: San Andreas</em> die volledig compatible is met de bestaande multiplayer mod <em>San Andreas Multiplayer.</em>
+      Een opkomende multiplayer modificatie voor <em>Grand Theft Auto: San Andreas</em> die volledig achterwaarts compatibel is 
+      met de bestaande multiplayer modificatie <em>San Andreas Multiplayer.</em>
     </p>
     <br />
     <p>
-      Dat betekent dat <strong>de bestaande SA:MP client en alle huidige SA:MP scripts te gebruiken zijn met open.mp</strong> en, daarnaast veel bug fixes met zich meedraagt zonder dat andere hulpprogramma's of trucjes nodig zijn.
+      Dit betekent dat <strong>de bestaande SA:MP cliënt en alle bestaande SA:MP scripts zullen werken met open.mp</strong> en,
+      in combinatie met dit, veel bugs ook gefixed zullen worden in de server's software zonder de benodigdheid van hacks en
+      andere alternatieven.
     </p>
     <p>
-      Als je benieuwd bent naar de release date of interesse hebt om een bijdrage te leveren, kan je <a href="https://www.burgershot.gg/showthread.php?tid=99">deze forumpost</a> lezen voor meer informatie.
+      Als u zich afvraagt wanneer de publieke publicatie is geplant of hoe u kan helpen met het bijdragen aan dit project, bekijk dan{" "}
+      <a href="https://www.burgershot.gg/showthread.php?tid=99">dit forum topic</a> voor meer informatie.
     </p>
     <h1>
       <a href="/faq">FAQ</a>
@@ -24,34 +28,76 @@ export const FAQ = () => (
     <h1>FAQ</h1>
     <hr />
     <h2>Wat is open.mp?</h2>
-    <p>open.mp (Open Multiplayer, OMP) is a vervangende multiplayer mod voor San Andreas, als reactie op de toenemende hoeveelheid van problemen rondom updates en moderatie in SA:MP. Het is volledig te gebruiken als vervanger, zodat gebruikers van de bestaande SA:MP client direct op OMP servers kunnen spelen naast OMP clients, met daarbij bovenop alle functionaliteit die eerder nog niet geïmplenteerd was.</p>
+    <p>
+      open.mp (Open Multiplayer, OMP) is een vervangende multiplayer modificatie voor San Andreas, gestart als reactie op de
+      spijtige toename van problemen met updates en het management van SA:MP. Het is volledig achterwaards compatibel, ontworpen
+      als een vervanging dat het mogelijk maakt om met bestaande SA:MP cliënten te spelen op OMP servers samen met nieuwe OMP cliënten, terwijl
+      updates en nieuwe features worden aangeboden die elders zijn gestrand.
+    </p>
     <hr />
-    <h2>Is dit een fork?</h2>
-    <p>Nee. Dit project is volledig herschreven, en combineert tientallen jaren kennis en ervaring. Er zijn eerdere pogingen geweest om een fork van SA:MP te maken, maar wij vinden dat hier twee problemen aan zitten:</p>
+    <h2>Is het een fork?</h2>
+    <p>
+      Nee. Dit is compleet opnieuw geschreven, profijt van een decennium van kennis en ervaring. Er zijn pogingen geweest
+      om SA:MP te forken, maar wij geloven dat deze twee belangrijke problemen hadden:
+    </p>
     <ol>
-      <li>Deze pogingen waren gebouwd op uitgelekte broncode van SA:MP. De makers van deze mods hadden geen wettelijk recht voor deze code, en liepen dus altijd achter op zowel moreel als rechterlijk niveau. Wij weigeren om met deze gelekte code te werken. Dit kan effect hebben op de ontwikkelingstijd, maar op de lange termijn vinden wij dit de juiste keuze.</li>
-      <li>Ze namen teveel hooi op de vork, zoals het vervanging van de scripting language, het vervangen en tegelijkertijd toevoegen van nieuwe features, of andere aanpassingen die niet goed samenwerkten. Dit verhindert dat servers met veel spelers en veel code meekomen, omdat het enorm veel werk vereist om zo veel functies te herschrijven naar een nieuwe infrastructuur. Wij gaan zeker nieuwe functionaliteit toevoegen en veranderingen maken, maar we willen er eerst voor zorgen dat we bestaande servers en spelers direct toegang kunnen geven tot open.mp.</li>
+      <li>
+        Ze waren gebasseerd op gelekte SA:MP broncode. De makers van deze modificaties hadden geen legaal recht tot deze broncode, 
+        en liepen dus risico, beide moraal en legaal. Wij weigeren ronduit om deze code te gebruiken. Dit belemmert
+        de ontwikkelings snelheid ietsjes, maar is de juiste zet op de lange termijn.
+      </li>
+      <li>
+        Ze probeerden te veel in één keer opnieuw uittevinden. Ofwel het vervangen van de scripting engine, of het verwijderen van features terwijl
+        nieuwe toegevoegd werden, of gewoon dingen veranderen op een tegenstrijdige manier. Dit weerhoud bestaande servers met enorme
+        codebases en playerbases om over te stappen, omdat ze sommige delen, zo niet alles, van hun broncode moeten herschrijven - een
+        potentieel grote onderneming. Wij menen het volledig om nieuwe features toe te voegen, en dingen te wijzigen, na verloop van tijd, maar wij zijn ook
+        gefocust op het ondersteunen van bestaande servers, wat het mogelijk maakt voor hun om onze broncode te gebruiken zonder die van hun te veranderen.
+      </li>
     </ol>
     <hr />
     <h2>Waarom doen jullie dit?</h2>
-    <p>Ondanks de pogingen om de ontwikkeling van SA:MP door te laten gaan, door middel van ideeën, door te vragen en zelfs door direct hulp te bieden in een community die al jaren lang om updates vroeg; er werd geen voortgang gemaakt. Het is waarschijnlijk dat dit komt doordat de moderators en ontwikkelaars geen verdere interesse hadden; dit is op zich geen probleem, maar er was niemand die het touw oppakte. In plaats van het overhandigen van verdere ontwikkeling aan een nieuw team besloot de eigenaar om alles voor zichzelf te nemen en met minimale inspanning proberen de boel bij elkaar te houden. Ondanks de sterke wil van de community is de originele mod tot heden amper verder ontwikkeld.</p>
+    <p>
+      Ondanks ontelbaar aantal pogingen om de ontwikkeling van SA:MP officieel vooruit te duwen, in de aard van suggesties, lastigvallen, en
+      voorstellen van hulp vanuit de bèta team; samen met een gemeenschap schreeuwend voor iets nieuws; was er geen progressie
+      geboekt. Dit was uitgebreid geloofd om simpelweg desinteresse op het vlak van modificatie leiderschap te tonen, dat
+      opzich geen groot probleem is, maar er was geen troonopvolging. Inplaats van het overhandigen van de ontwikkeling
+      aan degene die geinteresseerd zijn in het vervolgen van het werk aan de modificatie, wilde de oprichter simpelweg alles met zichzelf naar beneden
+      halen, terwijl hij blijkbaar zolang als mogelijk de modificatie in stand wilt houden met minimale moeite. Sommigen claimen dat dit voor
+      passieve inkomen redenen is, maar daar is geen bewijs voor. Ondanks veel interesse, en een sterke en overerfbare
+      gemeenschap, geloofde hij dat er nog maar 1-2 jaar over waren voor de modificatie, en dat de gemeenschap dat zo hard gewerkt had
+      om SA:MP te maken wat het vandaag is, geen voortzetting verdiende.
+    </p>
     <br />
-    <p>Wij besluiten de draad op te pakken.</p>
+    <p>Wij zijn het daarmee oneens.</p>
     <hr />
-    <h2>Omdat het "Open" Multiplayer is, wordt dit project open-source?</h2>
-    <p>Ja, dat is uiteindelijk de bedoeling. We zijn nu bezig de updates rond de ontwikkeling openbaar en transparent te maken, wat op zichzelf al een verbetering is. Zodra het mogelijk is gaan we open.mp open-source maken.</p>
+    <h2>Omdat het "Open" Multiplayer is genoemd, gaat het openbronsoftware worden?</h2>
+    <p>
+      Op den duur is dat het plan, ja. Voor nu zijn we aan het proberen om de ontwikkeling publiek te houden, wat betreft communicatie en
+      transparantie (Wat in zichzelf al een verbetering is), en stappen over op open-sourcing zoals en wanneer we kunnen, eenmaal
+      wanneer alles uitgezocht is en alles een plaatsje heeft gevonden.
+    </p>
     <hr />
-    <h2>Kan ik helpen?</h2>
-    <p>Via het forum kan je op de hoogte blijven. We hebben hier speciaal een topic voor gemaakt, en we zullen het updaten wanneer nieuwe informatie beschikbaar is. Ondanks dat het project wat eerder openbaar is geraakt dan onze bedoeling was, zijn we wel goed onderweg naar een eerste release. We kunnen alsnog veel hulp gebruiken. We willen je alvast bedanken voor je interesse! Je kan het betreffende topic hier vinden:</p>
+    <h2>Hoe kan ik helpen?</h2>
+    <p>
+      Houd je ogen open op het forum. We hebben een topic voor exact deze vraag, en zullen het actueel houden wanneer meer werk beschikbaar
+      is. Hoewel het project iets eerder onthult werd dan bedoeld, zijn we reeds goed onderweg voor een
+      initiële uitgave, maar dat betekent niet dat meer help niet enorm gewaardeerd wordt. Dank bij voorbaat voor
+      het interesse tonen, en geloven in het project:
+    </p>
     <br />
     <a href="https://www.burgershot.gg/showthread.php?tid=99">
-      <u>"How to help" topic (burgershot.gg)</u>
+      <u>"Hoe te helpen" topic (burgershot.gg)</u>
     </a>
     <hr />
     <h2>Wat is burgershot.gg?</h2>
-    <p>burgershot.gg is een vrijstaand gaming forum. Veel gebruikers zijn betrokken bij beide projecten, en sommige open.mp updates zullen hier gepost worden, maar uiteindelijk staan ze los van elkaar. Dit zijn niet de open.mp forums. Zodra de complete open.mp website online is zullen de twee projecten van elkaar losgetrokken worden (net zoals SA:MP ooit op GTAForums werd gehost).</p>
+    <p>
+      burgershot.gg is een gaming forum, niets meer niets minder. Veel van de mensen zijn betrokken in beiden, en sommige OMP ontwikkeling
+      en updates worden daar gepubliceerd, maar het zijn twee onafhankelijke projecten. Ze zijn niet de OMP forums, noch is OMP een
+      burgershot eigendom. Als eenmaal een complete OMP site online is, kunnen de twee losgemaakt worden van elkaar (Net als dat
+      SA:MP ooit gehost was onder GTAForums voordat haar eigen site online stond).
+    </p>
     <hr />
-    <h2>En OpenMP dan?</h2>
-    <p>Het Open Multi-Processing project gebruikt de naam "OpenMP" al, wij zijn "open.mp". Er zit geen overlap tussen.</p>
+    <h2>Hoe zit het met OpenMP?</h2>
+    <p>Het Open Multi-Processing project is "OpenMP", wij zijn "open.mp". Compleet verschillend.</p>
   </div>
 );
