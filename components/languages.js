9c7ed5633f0b05c2203f31efd2c89742ac182918
Barnaby Keene <hello@southcla.ws>
2019-04-18 20:48:48 +0100

    add Polish to language map and updated translation matrix

--- components/languages.js
+++ components/languages.js
@@ -14,6 +14,7 @@ export const LANGUAGES = {
   'ro': 'Romanian',
   'ru': 'Russian',
   'es': 'Spanish',
+  'pl': 'Polish',
 };
 
 export const loadLanguages = (initial, [ language, setLanguage ]) => {
