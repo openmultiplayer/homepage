0b86a11e54888d906687d05aaf79bd93fa5ac80a
Barnaby Keene <hello@southcla.ws>
2019-04-19 16:32:56 +0100

    satisfy linter for LanguageSelect component

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   plugins: ["react", "prettier"],
   rules: {
     "prettier/prettier": "error",
-    "react/prop-types": "off"
+    "react/prop-types": "off",
+    "import/prefer-default-export": "off"
   }
 };
--- components/LanguageSelect.jsx
+++ components/LanguageSelect.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import 'flag-icon-css/css/flag-icon.css';
+/* eslint-disable react/no-array-index-key */
+import React from "react";
+import "flag-icon-css/css/flag-icon.css";
 
-export const LanguageSelect = (props) => {
+export const LanguageSelect = ({ flags, selected, callback }) => {
   // no-op for single-language
-  if (props.flags.length === 1) {
+  if (flags.length === 1) {
     return <div />;
   }
 
   return (
     <div className="lang-list">
-      {props.flags.map((value, index, array) => {
+      {flags.map((value, index) => {
         let className = "lang-flag";
-        if (value == props.selected) {
+        if (value === selected) {
           className += " lang-flag-selected";
         }
         return (
-          <a href={`?lang=${value}`} key={index} className={className} onClick={(e) => props.callback(e, value)}>
-            <span className={`flag-icon flag-icon-${value}`}></span>
+          <a
+            href={`?lang=${value}`}
+            key={index}
+            className={className}
+            onClick={e => callback(e, value)}
+          >
+            <span className={`flag-icon flag-icon-${value}`} />
           </a>
         );
       })}
     </div>
   );
 };
-
