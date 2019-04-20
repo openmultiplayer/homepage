6365e47a4d0881872b1da99af7d334a07dea0d45
Autorojo <autorojo@ls-rp.es>
2019-04-20 18:49:11 -0300

    Update Spanish.jsx
    Just improving the text.

--- language/Spanish.jsx
+++ language/Spanish.jsx
@@ -32,13 +32,13 @@ export const FAQ = () => (
       open.mp (Open Multiplayer, OMP) es un mod multijugador sustituto para San Andreas, iniciado como respuesta al
       desafortunado aumento de problemas en actualizaciones y la administración de SA:MP. El lanzamiento inicial será 
       un reemplazo directo del software para el servidor solamente. Los clientes SA:MP ya existentes podrán conectarse
-      a este servidor. En el futuro, un nuevo cliente para open.mp estará disponible, permitiéndo lanzar más
+      a este servidor. En el futuro, un nuevo cliente para open.mp estará disponible, permitiendo lanzar más
       actualizaciones interesantes.
     </p>
     <hr />
     <h2>¿Es un fork/una bifurcación?</h2>
     <p>
-      No. Esta es una reescritura completa, tomando ventaja de decadas de conocimiento y experiencia. Han habido
+      No. Esta es una reescritura completa, tomando ventaja de décadas de conocimiento y experiencia. Han habido
       intentos de bifurcarse de SA:MP antes, pero creemos que estos tenian dos graves problemas:
     </p>
     <ol>
@@ -66,7 +66,7 @@ export const FAQ = () => (
       En vez de permitir que los demás pudieran contribuir al desarrollo del mod, el fundador simplemente quería traerse
       todo abajo con él mismo, mientras aparentemente mantenía las cosas cuando podía con un mínimo esfuerzo. Algunos
       decían que esto era por razones de ganancias pasivas, pero no hay evidencia de eso. A pesar del interés inmenso
-      y una fuerte comunidad, el pensaba que el mod solo tenía 1-2 años de vida, y la comunidad que trabajaba tan duro
+      y una fuerte comunidad, él pensaba que al mod le quedaban solo 1-2 años de vida, y la comunidad que trabajaba tan duro
       para hacer SA:MP lo que es hoy en día, no se merecía una continuación.
     </p>
     <br />
@@ -81,9 +81,9 @@ export const FAQ = () => (
     <hr />
     <h2>¿Cómo puedo ayudar?</h2>
     <p>
-      Manténte atento al foro. Tenemos un tema exactamente para esto, y tenemos la intención de mantenerlo
+      Permanece atento al foro. Tenemos un tema exactamente para esto, y tenemos la intención de mantenerlo
       actualizado mientras tengamos cosas que enseñar. Aunque el proyecto fue revelado mucho antes de lo esperado,
-      estamos en un muy bien camino para un lanzamiento inicial, pero eso no significa que mas ayuda no sea enormemente
+      estamos en un muy bien camino para un lanzamiento inicial, pero eso no significa que más ayuda no sea enormemente
       apreciada. Gracias de antemano por tener interés, y creer en el proyecto:
     </p>
     <br />
@@ -94,7 +94,7 @@ export const FAQ = () => (
     <h2>¿Qué es burgershot.gg?</h2>
     <p>
       burgershot.gg es un foro de gaming, nada más. Un montón de gente está involucrada en ambos, y algo del desarollo
-      y actualizaciones sobre OMP están siendo publicados ahí, pero son dos proyectos independientes. No són los foros OMP,
+      y actualizaciones sobre OMP están siendo publicados ahí, pero son dos proyectos independientes. No son los foros OMP,
       y tampoco es OMP propiedad de burgershot. Una vez que tengamos un sitio para OMP completo y andando, los dos podrán ser
       separados el uno del otro (muy así como SA:MP estuvo alguna vez hospedado en GTAForums antes de que tuvieran su propio 
       sitio).
