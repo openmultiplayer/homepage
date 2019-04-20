509fa3125cd681666fc5ce44e22b01465a7282a2
Barnaby Keene <hello@southcla.ws>
2019-04-20 11:57:42 +0100

    add a footer with a disclaimer

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { withRouter } from 'next/router';
 
 import { HeadContent } from '../components/HeadContent';
+import { FooterContent } from '../components/FooterContent';
 import Wordmark from '../components/icons/Wordmark';
 import Discord from '../components/icons/Discord';
 import Forum from '../components/icons/Forum';
@@ -45,6 +46,8 @@ const Index = ({
           </p>
         </section>
       </main>
+
+      <FooterContent />
     </div>
   );
 };
