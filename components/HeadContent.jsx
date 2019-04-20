2d50568c87bbf6c80e15886c57e3a043ba0effdf
Barnaby Keene <hello@southcla.ws>
2019-04-20 11:26:34 +0100

    resolved #65
    Specified the class of the parent div of the head content `.menu-content` so it can be controlled via the grid in `.container`
    
    Added style and grid specification for `.menu-content` so that it is positioned above the main content grid item.
    
    Specified an absolute size for the top-most row to be 60px instead of `auto`, this prevents the row from scaling down to nothing resluting in the language menu intersecting the logo/heading.
    
    Removed some unnecessary rules from `.lang-list` that are no longer needed due to the aforementioned changes.
    
    Also slightly adjusted the style of the language flag buttons with a slightly smaller border radius.

--- components/HeadContent.jsx
+++ components/HeadContent.jsx
@@ -8,7 +8,7 @@ const DESC =
   'Open Multiplayer - An upcoming multiplayer mod for Grand Theft Auto: San Andreas that is a fully backwards compatible substitute for SA:MP.';
 
 export const HeadContent = ({ title, ...rest }) => (
-  <div>
+  <div className="menu-content">
     <Head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
