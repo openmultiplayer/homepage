79951d0775418b82769c3bbbb41ac68c0c40b4f2
Carlos Menezes <36506580+carlos-menezes@users.noreply.github.com>
2019-04-22 09:39:55 +0100

    Update languages.js
    The language code for Portuguese os PT, as the language originated in Portugal.

--- components/languages.js
+++ components/languages.js
@@ -12,7 +12,7 @@ export const LANGUAGES = {
   it: 'Italian',
   lt: 'Lithuanian',
   no: 'Norwegian',
-  br: 'Portuguese',
+  pt: 'Portuguese',
   ro: 'Romanian',
   ru: 'Russian',
   es: 'Spanish',
