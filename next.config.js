5e198bfcf182f74abe6252b3a92961d3e398499e
Barnaby Keene <hello@southcla.ws>
2019-05-06 12:31:32 +0100

    add ga

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 const withCSS = require('@zeit/next-css');
-const withImages = require('next-images')
-module.exports = withCSS(withImages());
-
+const withImages = require('next-images');
+module.exports = withCSS(
+  withImages({
+    env: {
+      GA_ID: 'UA-78828365-7'
+    }
+  })
+);
---
+++ pages/_app.jsx
@@ -0,0 +1,17 @@
+import Router from 'next/router';
+import withGA from 'next-ga';
+
+import App, { Container } from 'next/app';
+
+export default withGA(process.env.GA_ID, Router)(
+  class extends App {
+    render() {
+      const { Component, pageProps } = this.props;
+      return (
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      );
+    }
+  }
+);
