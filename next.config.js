3eef728e642f52a310a878b776c821a797e286c6
Barnaby Keene <accounts@southcla.ws>
2019-07-15 16:15:22 +0100

    moved server side API to new Next.js 9.x api routes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+const { parsed: localEnv } = require('dotenv').config();
+const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
-module.exports = withCSS(
-  withImages({
-    env: {
-      GA_ID: 'UA-78828365-7'
-    }
-  })
-);
+
+module.exports = withPlugins([[withCSS], [withImages]], {
+  env: {
+    GA_ID: 'UA-78828365-7',
+    ...localEnv
+  }
+});
