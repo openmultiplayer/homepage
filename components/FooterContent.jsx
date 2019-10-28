40ba22a7dd1b7032d359ab1c23e6d3c14639ef85
Jamie Taylor <jamie@dr0id.in>
2019-10-28 09:54:23 +0000

    Recentres certain items (#128)
    The footer and the social links look odd when they're not in the middle

--- components/FooterContent.jsx
+++ components/FooterContent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FooterContent = () => (
   <footer>
-    <p>
+    <p className="center">
       open.mp is not associated with Rockstar Games, Take Two Interactive or San Andreas
       Multiplayer.
     </p>
