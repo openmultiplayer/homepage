8cc7b9a22a669c12f66deb73bf13e34c55aba4d2
Kristo Isberg <kristo.isberg@fleep.io>
2020-01-22 23:26:46 +0200

    Use ES6 dynamic import to load blog posts

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -1,46 +1,42 @@
-/* eslint-disable global-require */
-
 import React from 'react';
 import format from 'date-fns/format';
 import parseISO from 'date-fns/parseISO';
 
 import { Content, Paragraph } from '../../components/Typography';
 
-const Post = ({ slug }) => {
-  // eslint-disable-next-line import/no-dynamic-require
-  const post = require(`../../posts/${slug}.mdx`);
+const Post = ({ post }) => (
+  <>
+    <main>
+      <article>
+        <Content>
+          <aside>
+            <Paragraph>
+              Posted {format(parseISO(post.meta.date), 'yyyy-MM-dd')} by {post.meta.author}
+            </Paragraph>
+          </aside>
+          <post.default />
+        </Content>
+      </article>
+    </main>
 
-  return (
-    <>
-      <main>
-        <article>
-          <Content>
-            <aside>
-              <Paragraph>
-                Posted {format(parseISO(post.meta.date), 'yyyy-MM-dd')} by {post.meta.author}
-              </Paragraph>
-            </aside>
-            <post.default />
-          </Content>
-        </article>
-      </main>
+    <style jsx global>
+      {`
+        article {
+          padding: 10px;
+          max-width: 640px;
+          margin: auto;
+        }
+        pre {
+          text-align: left;
+        }
+      `}
+    </style>
+  </>
+);
 
-      <style jsx global>
-        {`
-          article {
-            padding: 10px;
-            max-width: 640px;
-            margin: auto;
-          }
-          pre {
-            text-align: left;
-          }
-        `}
-      </style>
-    </>
-  );
+Post.getInitialProps = async ({ query: { slug } }) => {
+  const post = await import(`../../posts/${slug}.mdx`);
+  return { post };
 };
-
-Post.getInitialProps = ({ query: { slug } }) => ({ slug });
 
 export default Post;
